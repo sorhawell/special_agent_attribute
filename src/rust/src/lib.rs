9b67061b3d682b97dd9fa1bbd184839bc77f1706
sorhawell <sorhawell@gmail.com>
2023-02-05 02:15:24 +0100

    POC special attribute error

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,16 +1,42 @@
 use extendr_api::prelude::*;
 
-/// Return string `"Hello world!"` to R.
 /// @export
 #[extendr]
-fn hello_world() -> &'static str {
-    "Hello world!"
+fn hello_extendr_error(fail_type: i32) -> Result<Robj> {
+    match fail_type {
+        1 => Err(extendr_api::Error::Other("imma extendr error".into())),
+        _ => Ok("nothing to see here, cary on".into()),
+    }
+}
+
+#[extendr]
+fn hello_string_error(fail_type: i32) -> std::result::Result<Robj, String> {
+    match fail_type {
+        1 => Err("imma string error msg".to_string().into()),
+        2 => Err(extendr_api::Error::Other("imma extendr error".to_string()))?,
+        3 => Err(format!("debug view {:?}", R!("sqrt(2)").unwrap())),
+        _ => Ok(42.into()),
+    }
+}
+
+/// @export
+#[extendr]
+fn hello_robj_error(fail_type: i32) -> std::result::Result<Robj, Robj> {
+    match fail_type {
+        1 => Err("imma string error msg".to_string().into()),
+        2 => Err(extendr_api::Error::Other("imma extendr error".to_string()))?,
+        3 => Err(R!("sqrt(2)").unwrap()),
+        _ => Ok(42.into()),
+    }
 }
 
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
 // See corresponding C code in `entrypoint.c`.
+
 extendr_module! {
     mod helloextendr;
-    fn hello_world;
+    fn hello_extendr_error;
+    fn hello_string_error;
+    fn hello_robj_error;
 }
--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,3 +1,40 @@
-test_that("Call to Rust function `hello_world()` works", {
-  expect_equal(hello_world(), "Hello world!")
+test_that("play with errors", {
+  
+  expect_equal(
+    list(
+     err = hello_extendr_error(1L),
+     ok = hello_extendr_error(0L)
+   ),
+    list(err = structure("imma extendr error", extendr_err = TRUE), 
+    ok = "nothing to see here, cary on")
+  )
+  
+  
+  expect_equal(
+    list(
+     err1 = hello_string_error(1L),
+     err2 = hello_string_error(2L),
+     err3 = hello_string_error(3L),
+     ok = hello_extendr_error(0L)
+  ),
+   list(err1 = structure("imma string error msg", extendr_err = TRUE), 
+    err2 = structure("imma extendr error", extendr_err = TRUE), 
+    err3 = structure("debug view 1.4142135623730951", extendr_err = TRUE), 
+    ok = "nothing to see here, cary on")
+  )
+  
+  
+   expect_equal(
+    list(
+     err1 = hello_robj_error(1L),
+     err2 = hello_robj_error(2L),
+     err3 = hello_robj_error(3L),
+     ok = hello_robj_error(0L)
+  ),
+   list(err1 = structure("imma string error msg", extendr_err = TRUE), 
+    err2 = structure("imma extendr error", extendr_err = TRUE), 
+    err3 = structure(1.4142135623731, extendr_err = TRUE), ok = 42L)
+  )
+    
+  
 })
